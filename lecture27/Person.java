248f54a9e2541b0acaea5fab7a676a9032c69556
Pratibha <pratibha.yadav97@gmail.com>
2023-04-06 23:26:26 +0530

    OOPs
    1. Memory allocation, 
    2. Parsing
    3. Constructor calling

---
+++ lecture27/Person.java
@@ -0,0 +1,25 @@
+package lecture27;
+
+public class Person {
+	String name;
+	int age;
+	
+	//Constructor
+//	public Person() {
+//		this.name="Motu";
+//		this.age=24;
+//	}
+	
+	
+	public Person(String name,int age) {
+	this.name=name;
+		this.age=age;
+	}
+	
+	public void fun() {
+		String name="Panchi";
+		int Aadhar_no=901928;
+		System.out.println(this.name+" "+this.age+" "+ Aadhar_no);
+		
+	}
+}
---
+++ lecture27/Person_client.java
@@ -0,0 +1,25 @@
+package lecture27;
+
+public class Person_client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Constructor
+		//Memory allocation
+		//Parsing
+		
+		Person p = new Person("Karan",25);
+		p.name="Kaju";
+		p.age=23;
+		p.fun(); 
+		
+		Person p1=new Person("Kumar",24);
+		p1.name="Kunal";
+		p1.age=45;
+		p1.fun();
+		
+		Person p2 = new Person("Karan",25);
+		p2.fun(); 
+	}
+
+}
