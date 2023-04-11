eadf404464f65cb37b18ccda0aa0df8082502416
Pratibha <pratibha.yadav97@gmail.com>
2023-04-11 20:39:18 +0530

    Assignment operator

--- lecture01/Assignment_operater.java
+++ lecture01/Assignment_operater.java
@@ -5,7 +5,7 @@ public class Assignment_operater {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x = 1;
-		System.out.println(x);
+		System.out.print(x);
 	}
 
 }
