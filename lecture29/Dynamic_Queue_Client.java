a6e9c09e41712f166668754b736c7c752a0d38a1
Pratibha <pratibha.yadav97@gmail.com>
2023-04-13 02:13:20 +0530

    Dynamic Queue
    Inheritance

--- lecture29/Dynamic_Queue_Client.java
+++ lecture29/Dynamic_Queue_Client.java
@@ -5,6 +5,7 @@ public class Dynamic_Queue_Client {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Dynamic_Queue dq=new Dynamic_Queue();
+		
 		dq.Enqueue(10);
 		dq.Enqueue(20);
 		dq.Enqueue(30);
