2017af78b63d63e33f0b86dce17295c06edd7ced
Pratibha <pratibha.yadav97@gmail.com>
2023-05-03 23:36:35 +0530

    Inverted Star mountain
    * * * * * * * * * 
      * * * * * * * 
        * * * * * 
          * * * 
            *

--- lecture02/Pattern_question_10.java
+++ lecture02/Pattern_question_10.java
@@ -4,6 +4,7 @@ public class Pattern_question_10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int row=1;
