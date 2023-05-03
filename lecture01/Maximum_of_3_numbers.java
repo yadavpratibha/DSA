44dad3722544ab2050b7b82f81f294a8eb6d2401
Pratibha <pratibha.yadav97@gmail.com>
2023-05-03 23:31:23 +0530

    Operators, Loop

--- lecture01/Maximum_of_3_numbers.java
+++ lecture01/Maximum_of_3_numbers.java
@@ -6,6 +6,7 @@ public class Maximum_of_3_numbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
