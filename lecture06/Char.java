e07ee4f2c95a844980095af1a334f1c617c04257
Pratibha <pratibha.yadav97@gmail.com>
2023-05-15 23:25:48 +0530

    Character

---
+++ lecture06/Char.java
@@ -0,0 +1,15 @@
+package lecture06;
+
+public class Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='a';
+		System.out.println(ch);
+		System.out.println((int)(ch));
+		ch++;
+		System.out.println(ch);
+		System.out.println(++ch);
+	}
+
+}
