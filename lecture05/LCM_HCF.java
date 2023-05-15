c278bf9c1dbd73da513bbbec5b7b6fab571111ee
Pratibha <pratibha.yadav97@gmail.com>
2023-05-15 23:01:58 +0530

    LCM*HCF=n1*n2

---
+++ lecture05/LCM_HCF.java
@@ -0,0 +1,11 @@
+package lecture05;
+import java.util.Scanner;
+public class LCM_HCF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =  new Scanner(System.in);
+		
+	}
+
+}
