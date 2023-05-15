1af625ad86aa10c1798a657e33d64fe50a8193ea
Pratibha <pratibha.yadav97@gmail.com>
2023-05-15 23:02:53 +0530

    Loop, Operator

---
+++ lecture05/Sum_of_digits.java
@@ -0,0 +1,18 @@
+package lecture05;
+import java.util.*;
+public class Sum_of_digits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum=0;
+		while(n!=0) {
+			int rem=n%10;
+			n=n/10;
+			sum=sum+rem;
+		}
+		System.out.println(sum);
+	}
+
+}
