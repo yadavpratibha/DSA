1e67de36fe59cf050a65a4b1417b911efbf84bcf
Pratibha <pratibha.yadav97@gmail.com>
2023-05-19 19:56:24 +0530

    Functions

---
+++ lecture07/Function_demo3.java
@@ -0,0 +1,18 @@
+package lecture07;
+
+public class Function_demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8;
+		int b=7;
+		System.out.println("hey");
+		System.out.println(Add(a,b));
+		System.out.println("Bye");
+	}
+
+	public static int Add(int a, int b) {
+		int c=a+b;
+		return c;
+	}
+}
