ce0cb36bcd053e2be0e50967bc2b9bf75b5dc194
Pratibha <pratibha.yadav97@gmail.com>
2023-05-22 17:45:24 +0530

    Arrays

---
+++ lecture08/Array_swap_two_number_3.java
@@ -0,0 +1,32 @@
+package lecture08;
+import java.util.*;
+public class Array_swap_two_number_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int[] other = {100,200,300,400,500};
+		swap(arr,other);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		for(int i=0;i<other.length;i++) {
+			System.out.print(other[i]+" ");
+		}
+	}
+	//swap elements of arr with elements of others
+	public static void swap(int[] a, int[] oth) { 
+		for(int i=0;i<a.length;i++) {
+			int temp = a[i];
+			a[i] = oth[i];
+			oth[i] = temp;
+		}
+	}
+
+}
