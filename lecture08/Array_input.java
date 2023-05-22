22b52e6f18fe43ac0025de7ce09a5d9d0f3a8097
Pratibha <pratibha.yadav97@gmail.com>
2023-05-22 17:44:33 +0530

    Arrays

---
+++ lecture08/Array_input.java
@@ -0,0 +1,25 @@
+package lecture08;
+
+import java.util.*;
+
+public class Array_input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); //input size of array
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Display(arr);
+	}
+
+	public static void Display(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
