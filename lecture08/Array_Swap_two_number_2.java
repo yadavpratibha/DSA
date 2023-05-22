1bdc81b29490f4ae61805a440a48ec37ac97ff51
Pratibha <pratibha.yadav97@gmail.com>
2023-05-22 17:44:53 +0530

    Arrays

---
+++ lecture08/Array_Swap_two_number_2.java
@@ -0,0 +1,23 @@
+package lecture08;
+import java.util.*;
+public class Array_Swap_two_number_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		swap(arr);
+		System.out.print(arr[0]+" "+arr[1]);
+	}
+	public static void swap(int[] arr) {
+		// swap two numbers in their indexes
+		int temp = arr[0];
+		arr[0] = arr[1];
+		arr[1] = temp;
+	}
+
+}
