24d3f7a721bd5361165fcde00c60b2801f8680cb
Pratibha <pratibha.yadav97@gmail.com>
2023-05-23 21:44:32 +0530

    Arrays

---
+++ lecture09/Find_maximum_of_given_number2.java
@@ -0,0 +1,22 @@
+package lecture09;
+
+public class Find_maximum_of_given_number2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {2,9,7,11,8};
+		System.out.println(Max(arr,5));
+	}
+	
+	public static int Max(int[] arr, int j) {
+			int max =Integer.MIN_VALUE;
+			for(int i=1;i<arr.length;i++) {
+				if(arr[i]>max) {
+					max=arr[i];
+				}
+			
+			}
+			return max;
+		}
+	}
+
