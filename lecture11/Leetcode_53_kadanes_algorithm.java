17012058e81bcc160fed3df372dbcf0869b0a04f
Pratibha <pratibha.yadav97@gmail.com>
2023-05-29 17:01:16 +0530

    Array

---
+++ lecture11/Leetcode_53_kadanes_algorithm.java
@@ -0,0 +1,24 @@
+package lecture11;
+
+public class Leetcode_53_kadanes_algorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {2,3,-18,7,-1,3};
+		System.out.println(MaximumSum(arr));
+	}
+	public static int MaximumSum(int[] arr) {
+		int ans = Integer.MIN_VALUE;
+		int sum=0;
+		for(int i =0;i<arr.length;i++) {
+				sum+=arr[i];
+				ans=Math.max(ans, sum);
+				if(sum<0) {
+					sum=0;
+				}
+			
+		}
+		return ans;
+	}
+
+}
