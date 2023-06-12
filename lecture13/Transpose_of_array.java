78795fb2bc02341b84814e96f34b2f417a5a898b
Pratibha <pratibha.yadav97@gmail.com>
2023-06-13 01:52:21 +0530

    Array

---
+++ lecture13/Transpose_of_array.java
@@ -0,0 +1,39 @@
+package lecture13;
+
+import java.util.*;
+
+public class Transpose_of_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		Transpose(arr);
+		// print resultant array
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+//function for transpose of input matrix
+	public static void Transpose(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			//swap elements only on one side of the diagonal so j=i+1
+			for (int j = i + 1; j < arr[0].length; j++) {
+				int temp = arr[i][j]; //swap elements at arr[i][j] and arr[j][i]
+				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
+
+			}
+		}
+	}
+}
