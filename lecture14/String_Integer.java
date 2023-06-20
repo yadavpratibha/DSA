498e71794be33bf3139f0b4d568d8496ed28d252
Pratibha <pratibha.yadav97@gmail.com>
2023-06-20 11:35:16 +0530

    String

---
+++ lecture14/String_Integer.java
@@ -0,0 +1,14 @@
+package lecture14;
+
+public class String_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello";
+		System.out.println(s+10+30); //string + anything is always equal to string
+		System.out.println(s+(10+30));
+		System.out.println(s.substring(1, 4));
+		System.out.println(s.substring(2));
+	}
+
+}
