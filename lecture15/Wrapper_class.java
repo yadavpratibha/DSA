098e837178e2dc0f0a8a756a3c3a49460803b33f
Pratibha <pratibha.yadav97@gmail.com>
2023-06-22 19:54:18 +0530

    ArrayList

---
+++ lecture15/Wrapper_class.java
@@ -0,0 +1,21 @@
+package lecture15;
+
+public class Wrapper_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a = 89;
+		int a1 = 89;
+		System.out.println(a);
+		System.out.println(a1);
+		a=a1; //Auto-boxing
+		int b = a; //unBoxing
+		Integer a2=45;
+		Integer a3=45;
+		Integer b2=145;
+		Integer b3=145;
+		System.out.println(a2==a3);
+		System.out.println(b2==b3);
+	}
+
+}
