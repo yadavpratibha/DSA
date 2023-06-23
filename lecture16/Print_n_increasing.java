b722752e60dc1742680fafe2b9bafca20c89c674
Pratibha <pratibha.yadav97@gmail.com>
2023-06-23 21:01:35 +0530

    Recursion

---
+++ lecture16/Print_n_increasing.java
@@ -0,0 +1,17 @@
+package lecture16;
+
+public class Print_n_increasing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		print(n);
+	}
+	public static void print(int n) {
+		if(n==0) {
+			return;
+		}
+		print(n-1);
+		System.out.println(n);
+	}
+}
