45ac6a01fb98aaabf073b8969c2a97f150fc7515
Pratibha <pratibha.yadav97@gmail.com>
2023-06-26 09:34:28 +0530

    Recursion

---
+++ lecture17/Assignment_First_Index.java
@@ -0,0 +1,31 @@
+package lecture17;
+import java.util.Scanner;
+public class Assignment_First_Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int m=sc.nextInt();  //item
+		FirstIndex(arr, 0, m);
+	}
+	
+	public static void FirstIndex(int[] arr,int idx,int m) {
+		if(idx==arr.length) {
+			System.out.println(-1);
+			return;
+		}
+		if(arr[idx]==m) {
+			System.out.println(idx);
+			return ;
+		}
+		
+		FirstIndex(arr, idx+1, m);
+		
+	}
+
+}
