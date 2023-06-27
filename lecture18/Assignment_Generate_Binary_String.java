a289ae69aba09d0c87b927da9d814172b4dc0c72
Pratibha <pratibha.yadav97@gmail.com>
2023-06-28 00:16:35 +0530

    Recursion

---
+++ lecture18/Assignment_Generate_Binary_String.java
@@ -0,0 +1,30 @@
+package lecture18;
+
+import java.util.Scanner;
+
+public class Assignment_Generate_Binary_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       Scanner sc = new Scanner(System.in);
+	        int t = sc.nextInt();
+	        while (t-- > 0) {
+	            String s = sc.next();
+	            BinaryStrings(s, "");
+	            System.out.println();
+	        }
+	    }
+	    public static void BinaryStrings(String ques, String ans) {
+	        if (ques.length() == 0) {
+	            System.out.print(ans + " ");
+	            return;
+	        }
+	        char ch = ques.charAt(0);
+	        if (ch == '?') {
+	            BinaryStrings(ques.substring(1), ans + '0');
+	            BinaryStrings(ques.substring(1), ans + '1');
+	        } else {
+	            BinaryStrings(ques.substring(1), ans + ch);
+	        }
+	    }
+	}
